2e4137e3ca614bbb2bbd91360165f27b59a0c8ef
Alex <2703parker@gmail.com>
2025-02-26 23:59:10 -0600

    switch

--- Assignment2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ Assignment2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -1,6 +1,9 @@
 package edu.wisc.cs.sdn.vnet.sw;
 
 import net.floodlightcontroller.packet.Ethernet;
+
+import java.util.HashMap;
+
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
@@ -19,6 +22,9 @@ public class Switch extends Device
 		super(host,logfile);
 	}
 
+	private HashMap<String, switchEntry> switchMap = new HashMap<>();
+
+	private boolean debug = false;
 	/**
 	 * Handle an Ethernet packet received on a specific interface.
 	 * @param etherPacket the Ethernet packet that was received
@@ -29,9 +35,62 @@ public class Switch extends Device
 		System.out.println("*** -> Received packet: " +
 				etherPacket.toString().replace("\n", "\n\t"));
 		
-		/********************************************************************/
-		/* TODO: Handle packets                                             */
+		String srcString = etherPacket.getSourceMAC().toString();
+		String dstString = etherPacket.getDestinationMAC().toString();
+
+		switchEntry entry = switchMap.get(dstString);
+		if (entry != null) {
+			if (entry.isExpired()) {
+				if (debug) System.out.println("Expired entry");
+				switchMap.remove(dstString);
+				broadcast(etherPacket, inIface);
+			} else {
+				if (debug) System.out.println("Sending packet");
+				sendPacket(etherPacket, entry.getIface());
+			}
+		} else {
+			if (debug) System.out.println("Broadcasting");
+			broadcast(etherPacket, inIface);
+		}
 		
-		/********************************************************************/
+		entry = switchMap.get(srcString);
+		if (entry == null) {
+			if (debug) System.out.println("Adding mapping");
+			switchMap.put(srcString, new switchEntry(inIface));
+		} else {
+			entry.updateTime();
+		}
+
+		if (debug) System.out.println(System.currentTimeMillis());
+	}
+
+	private void broadcast(Ethernet etherPacket, Iface inIface) {
+		for (Iface curIface : interfaces.values()) {
+			if (!curIface.equals(inIface)) {
+				sendPacket(etherPacket, curIface);
+			}
+		}
+	}
+}
+
+class switchEntry {
+	private long time;
+	private Iface iface;
+
+	public switchEntry(Iface iface) {
+		this.time = System.currentTimeMillis();
+		this.iface = iface;
+	}
+
+	public boolean isExpired() {
+		return ((System.currentTimeMillis() - this.time) > 15000);
+	}
+
+	public Iface getIface() {
+		return this.iface;
+	}
+
+	public void updateTime() {
+		this.time = System.currentTimeMillis();
 	}
 }
